a9c303f43c1e93b42d2fc44ad98ee15203e52846
Janeks111 <90975618+Janeks111@users.noreply.github.com>
2024-03-29 17:28:22 +0000

    made changes in placeholders

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,18 +1,44 @@
 import { MdLink } from "react-icons/md";
 import { IoLogoGithub } from "react-icons/io";
 
-import project1 from "/8.png";
-import project2 from "/2.jpg";
-import project3 from "/3.jpg";
-import project4 from "/4.jpg";
-import project5 from "/5.jpg";
+import project1 from "/1.png";
+import project2 from "/2.png";
+import project3 from "/3.png";
+import project4 from "/4.png";
+import project5 from "/5.png";
 
 const worksData = [
-  { icon: project1, name: "project 1" },
-  { icon: project2, name: "project 2" },
-  { icon: project3, name: "project 3" },
-  { icon: project4, name: "project 4" },
-  { icon: project5, name: "project 5" },
+  {
+    icon: project1,
+    name: "project 1",
+    liveLink: "https://pursuit-of-wits.netlify.app",
+    githubLink: "https://github.com/annabrisland/pursuit-of-wits",
+  },
+  {
+    icon: project2,
+    name: "project 2",
+    liveLink: "https://kevinstaresdarbon.github.io/NoFOMO/",
+    githubLink:
+      "https://github.com/kevinstaresdarbon/NoFOMO?tab=readme-ov-file",
+  },
+  {
+    icon: project3,
+    name: "project 3",
+    liveLink: "https://janeks111.github.io/Quiz-with-JavaScript/index.html",
+    githubLink: "https://github.com/Janeks111/Quiz-with-JavaScript",
+  },
+  {
+    icon: project4,
+    name: "project 4",
+    liveLink: "https://janeks111.github.io/Working-Day-Planner/",
+    githubLink: "https://github.com/Janeks111/Working-Day-Planner",
+  },
+  {
+    icon: project5,
+    name: "project 5",
+    liveLink: "https://janeks111.github.io/Random-password-generator/",
+    githubLink: "https://github.com/Janeks111/Random-password-generator",
+  },
 ];
 
 const Works = () => {
@@ -24,7 +50,7 @@ const Works = () => {
       {/* heading */}
       <div className="right w-fit flex flex-col sm:flex-row sm:justify-center sm:items-center items-center gap-8 sm:gap-2 space-y-4 sm:space-y-0 space-x-0 mt-[2.2rem]">
         <div className="rotate-[-90deg] sm:rotate-0 min-w-[120px] flex items-center justify-center uppercase text-[#5e5e5e] dark:text-[#9e9e9e] text-[20px]">
-          my works
+          my projects
         </div>
         <div className="line h-[40px] w-[2px] sm:w-[40px] sm:h-[2px] bg-[#5e5e5e] dark:bg-[#9e9e9e]"></div>
       </div>
@@ -33,7 +59,7 @@ const Works = () => {
       <div className="flex sm:flex-col items-start sm:items-center w-full sm:px-4 sm:gap-8">
         <div className="left flex-1 ">
           <h2 className="text-[35px] font-bold text-black dark:text-white">
-            See My Works
+            See My Projects
           </h2>
           <p className="w-[78%] sm:w-[90%] text-[#5e5e5e] dark:text-[#9e9e9e]">
             Explore my portfolio to witness the fusion of creativity and
@@ -45,28 +71,44 @@ const Works = () => {
         </div>
         <div className="right flex-[2] sm:w-full flex flex-col gap-4">
           <div className="skills text-justify w-[92%] sm:w-full text-[#5e5e5e] dark:text-[#9e9e9e] columns-3 sm:columns-1 space-y-4">
-            {worksData?.map((data) => (
+            {worksData?.map((data, index) => (
               <div
-                key={data.icon}
+                key={index}
                 className="relative group overflow-hidden shadow-md"
               >
-                <img
-                  className="rounded-md overflow-hidden "
-                  src={data.icon}
-                  alt=""
-                />
+                <a
+                  href={data.liveLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    className="rounded-md overflow-hidden"
+                    src={data.icon}
+                    alt=""
+                  />
+                </a>
                 <div className="absolute bottom-[-50px] capitalize group-hover:bottom-4 rounded-[4px] left-[50%] translate-x-[-50%] p-1 px-3   w-[90%] flex items-center justify-between">
                   <p className="text-black font-medium bg-[#fff] shadow-md p-1 px-2 rounded-full">
                     {data.name}
                   </p>
                   <div className=" flex items-center gap-3">
-                    <a href="#" title="Link">
+                    <a
+                      href={data.liveLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      title="Live Link"
+                    >
                       <MdLink
                         size={28}
                         className=" rounded-full text-black text-[18px] bg-[#fff] shadow-md p-1"
                       />
                     </a>
-                    <a href="#" title="Github">
+                    <a
+                      href={data.githubLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      title="GitHub Link"
+                    >
                       <IoLogoGithub
                         size={28}
                         className=" rounded-full text-black text-[18px] bg-[#fff] shadow-md p-1"
@@ -77,7 +119,7 @@ const Works = () => {
               </div>
             ))}
             <p className=" text-center capitalize text-black dark:text-white text-[16px] font-semibold cursor-pointer">
-              <a href="#skills" title="Skills">
+              <a href="https://github.com/Janeks111" title="GitHub">
                 See more <span className=" text-red-500 text-[20px]">.</span>
               </a>
             </p>
